08ea48681c53b63fe169a1710ab2dd64f20fc8d0
Lockstar14 <pomincukr@gmail.com>
2024-06-25 05:57:34 +0500

    commit

--- data/state-manager.js
+++ data/state-manager.js
@@ -1,25 +1,37 @@
-const _state ={
-    points:{
-   miss: 2,
-   catch:3
-}}
+const _state = {
+    points: {
+        miss: 2,
+        catch: 3
+    },
+    settings: {
+        pointsToLose: 5,
+        pointsToWin: 5,
+        gridSize: {
+            width: 4,
+            height: 4
+        },
+    }
+}
 
-let _observer = () => {}
+let _observer = () => { }
 
 export function subscribe(subscriber) {
-_observer = subscriber
+    _observer = subscriber
 }
 
 
 //getter/selector/query
-export const getPoints = function(){
-    return{
+export const getPoints = function () {
+    return {
         miss: _state.points.miss,
         catch: _state.points.catch
     }
 }
 
-setInterval(()=>{
+setInterval(() => {
     _state.points.miss++
-    observer()
-},1000)
+    if (_state.points.miss === _state.settings.pointsToLose){
+        clearInterval(intervalId);
+    }
+        observer()
+}, 1000)
