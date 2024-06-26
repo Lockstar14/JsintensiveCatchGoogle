b7d79738ec6f3d709129a9ac40dca25fcf6caf36
Lockstar14 <pomincukr@gmail.com>
2024-06-26 17:44:57 +0500

    one more more more commit)

--- data/state-manager.js
+++ data/state-manager.js
@@ -3,15 +3,15 @@ import { GAME_STATUSES } from "./constants.js"
 const _state = {
     gameStatus: GAME_STATUSES.IN_PROGRESS,
     points: {
-        miss: 2,
-        catch: 3
+        miss: 0,
+        catch: 0
     },
     settings: {
         pointsToLose: 30,
         pointsToWin: 5,
         gridSize: {
-            width: 6,
-            height: 6
+            width: 5,
+            height: 5
         },
     },
    googlePosition: {
@@ -24,14 +24,29 @@ let _observer = () => { }
 export function subscribe(subscriber) {
     _observer = subscriber
 }
-
-
+function _getRandomInt(max){
+    return Math.floor(Math.random()*Math.floor(max))
+}
+function _moveGoogleToRandomPosition(){
+    const newX = _getRandomInt(_state.settings.gridSize.width)
+    const newY = _getRandomInt(_state.settings.gridSize.height)
+    if(newX === _state.googlePosition.x && newY === _state.googlePosition.y){
+        _moveGoogleToRandomPosition()
+        return;
+    }
+    _state.googlePosition.x = newX
+    _state.googlePosition.y = newY
+}
+let _intervalId
 function _play(){
-    let _intervalId = setInterval(()=>{
+    _intervalId = setInterval(()=>{
         _state.points.miss++
         if(_state.points.miss === _state.settings.pointsToLose){
             clearInterval(_intervalId)
             _state.gameStatus = GAME_STATUSES.LOSE
+        }
+        else{
+            _moveGoogleToRandomPosition()
         }
         _observer()
     },1000)
@@ -58,10 +73,30 @@ export function getGridSize(){
         width: _state.settings.gridSize.width
     }
 }
+
+export function getGooglePosition(){
+    return {
+        x: _state.googlePosition.x,
+        y: _state.googlePosition.y
+    }
+}
 export function playAgain(){
     _state.gameStatus = GAME_STATUSES.IN_PROGRESS
     _state.points.catch = 0
     _state.points.miss = 0
     _play()
+    _observer()
+}
+export function catchGoogle(){
+    _state.points.catch++
+    if(_state.points.catch === _state.settings.pointsToWin){
+        clearInterval(_intervalId)
+        _state.gameStatus = GAME_STATUSES.WIN
+    }
+    else{
+        _moveGoogleToRandomPosition()
+        clearInterval(_intervalId)
+        _play()
+    }
     _observer()
 }
--- ui/Grid/Grid.component.js
+++ ui/Grid/Grid.component.js
@@ -1,7 +1,8 @@
-import { getGridSize } from "../../data/state-manager.js"
+import { catchGoogle, getGooglePosition, getGridSize } from "../../data/state-manager.js"
 export function GridComponent(){
     const element = document.createElement('table')
     const gridSize = getGridSize()
+    const googlePosition = getGooglePosition()
     for (let y=0; y<gridSize.height; y++){
         const rowElement = document.createElement("tr")
         for(let x = 0; x<gridSize.width;x++){
@@ -9,8 +10,14 @@ export function GridComponent(){
             // const cellElement = CellComponent(x,y)
             const cellElement = document.createElement("td")
             cellElement.append()
-            if(true){
-                cellElement.append("G")   
+            if(googlePosition.x===x && googlePosition.y === y){
+                // todo: const googleElement = Google()
+                const googleElement = document.createElement('span')
+                googleElement.append("- G -")
+                googleElement.addEventListener("click", ()=>{
+                    catchGoogle()
+                })
+                cellElement.append(googleElement)   
             }
             rowElement.append(cellElement)
         }
